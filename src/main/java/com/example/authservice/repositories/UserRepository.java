63b3c7264cf6aadf0b039450d648937d2db236d4
ozgee <ozgeeodabassg@gmail.com>
2025-07-31 23:48:15 +0300

    user repository

--- src/main/java/com/example/authservice/repositories/UserRepository.java
+++ src/main/java/com/example/authservice/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.authservice.repositories;
 
 import com.example.authservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByUsername(String username);
 
+    Optional<User> findByUsernameAndDeletedIsFalse(String username);
 }
