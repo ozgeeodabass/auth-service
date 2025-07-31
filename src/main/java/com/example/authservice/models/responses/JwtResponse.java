cb46467f30ce12bf8f9f75ee5ea6bf6541ae08ef
ozgee <ozgeeodabassg@gmail.com>
2025-07-31 23:48:58 +0300

    login request and jwt response classes for sign in endpoint

---
+++ src/main/java/com/example/authservice/models/responses/JwtResponse.java
@@ -0,0 +1,18 @@
+package com.example.authservice.models.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private UUID id;
+    private String username;
+    private String email;
+    private String gender;
+    private String name;
+    private String phoneNumber;
+}
