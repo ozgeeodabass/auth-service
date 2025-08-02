02cdf0d57c103377d20bf538434c81ae1f9fbd6a
ozgee <ozgeeodabassg@gmail.com>
2025-08-02 17:14:23 +0300

    sign in endpoint

--- src/main/java/com/example/authservice/controllers/AuthController.java
+++ src/main/java/com/example/authservice/controllers/AuthController.java
@@ -1,12 +1,17 @@
 package com.example.authservice.controllers;
 
 import com.example.authservice.models.User;
+import com.example.authservice.models.requests.LoginRequest;
 import com.example.authservice.models.requests.RegisterRequest;
+import com.example.authservice.models.responses.JwtResponse;
 import com.example.authservice.services.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +40,26 @@ public class AuthController {
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>("INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //This endpoint receives a LoginRequest, authenticates the user, and returns a JWT token inside response.
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        try {
+            JwtResponse response = authService.authenticateUser(loginRequest);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            ///e.printStackTrace();
+            if (e.getClass().equals(AccessDeniedException.class)) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+            }else if(e.getClass().equals(UsernameNotFoundException.class)){
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            }
+            else if(e.getClass().equals(BadCredentialsException.class)){
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/example/authservice/services/AuthService.java
+++ src/main/java/com/example/authservice/services/AuthService.java
@@ -1,11 +1,24 @@
 package com.example.authservice.services;
 
 import com.example.authservice.models.User;
+import com.example.authservice.models.requests.LoginRequest;
 import com.example.authservice.models.requests.RegisterRequest;
+import com.example.authservice.models.responses.JwtResponse;
 import com.example.authservice.repositories.UserRepository;
+import com.example.authservice.security.jwt.JwtUtils;
+import com.example.authservice.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -15,6 +28,12 @@ public class AuthService {
 
     @Autowired
     private PasswordEncoder encoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
 
     public User registerUser(RegisterRequest registerRequest) {
         if (userRepository.existsByUsername(registerRequest.getUsername()))
@@ -32,5 +51,41 @@ public class AuthService {
                 .name(!registerRequest.getName().isEmpty()?registerRequest.getName():null)
                 .phoneNumber(!registerRequest.getPhoneNumber().isEmpty()?registerRequest.getPhoneNumber():null)
                 .build();
+    }
+
+    public JwtResponse authenticateUser(LoginRequest loginRequest) {
+        //first, check if the user exists and is not marked as deleted
+        Optional<User> user = userRepository.findByUsernameAndDeletedIsFalse(loginRequest.getUsername());
+        if(user.isEmpty())
+            throw new UsernameNotFoundException("USER_NOT_FOUND");
+
+        try{
+            //try to authenticate the user using username and password
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+            //set the authenticated user in the security context
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //generate JWT token
+            String jwt = jwtUtils.generateJwtToken(authentication);
+
+            //get authenticated user details
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toList();
+
+            //return token and user details
+            return new JwtResponse(jwt,
+                    "Bearer",
+                    userDetails.getId(),
+                    userDetails.getUsername(),
+                    userDetails.getEmail(),
+                    userDetails.getGender(),
+                    userDetails.getName(),
+                    userDetails.getPhoneNumber());
+        }catch (BadCredentialsException e){
+            throw new BadCredentialsException("WRONG_PASSWORD");
+        }
     }
 }
